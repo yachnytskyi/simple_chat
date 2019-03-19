569f723717901adc6d31d7c99d9a83bd7c660544
yachnytskyi <yachnytskyi1992@gmail.com>
2019-03-18 23:32:02 -0700

    Fixe ChatHub, package and wwwroot/index

--- simple_chat/ChatHub.cs
+++ simple_chat/ChatHub.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace simple_chat
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message, string userName)
+        public async Task Send(string message)
         {
-            await this.Clients.All.SendAsync("Send", message, userName);
+            await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} entered chat");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} left chat chat");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
